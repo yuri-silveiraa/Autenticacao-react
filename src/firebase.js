412abeb63f77823152576eada9d9b3c44f8687e4
Yuri Silveira <yuri.pires.silveira@gmail.com>
2023-03-23 09:39:37 -0300

    adicionando inputs

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDH0quwdKKWY3G0GF58FV2eYp5uVI9-Uz4",
+  authDomain: "auth-b65d8.firebaseapp.com",
+  projectId: "auth-b65d8",
+  storageBucket: "auth-b65d8.appspot.com",
+  messagingSenderId: "876190454757",
+  appId: "1:876190454757:web:8e755ad1871cc25439950c"
+};
+
+export const app = initializeApp(firebaseConfig);
---
+++ src/pages/Signup.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export const Singup = () => {
+    return (
+        <div className="container">
+            <h2>Singup</h2>
+            <form>
+                <label>Email</label>
+                <input type="email" />
+                <input type="password"/>
+            </form>
+        </div>
+    )
+}
