f4fc8fe03e4f22edbcc34bf32c53262cf4d22b01
Yuri Silveira <yuri.pires.silveira@gmail.com>
2023-03-23 10:10:53 -0300

    adicionando os inputs

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React,{ useState } from "react";
 
 
 export const Singup = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
     return (
         <div className="container">
             <h2>Singup</h2>
             <form>
                 <label>Email</label>
-                <input type="email" />
-                <input type="password"/>
+                <input 
+                    type="email"
+                    value={email}
+                    onChange={(element) => {setEmail(element.target.value)}}
+                />
+
+                <label>Password</label>
+                <input 
+                    type="password"
+                    value={password}
+                    onChange={(element) => {setPassword(element.target.value)}}
+                />
+
+                <label>Password Confirm</label>
+                <input 
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(element) => {setConfirmPassword(element.target.target)}}
+                />
+
+                <button className="button-block" type="submit">Singup</button>
             </form>
         </div>
     )
